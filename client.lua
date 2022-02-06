08c7e6998783332325ebbe39cc46887b3b404d3d
Jay <80186604+jay-fivem@users.noreply.github.com>
2022-02-06 16:15:39 +1030

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,19 @@
+local zone = PolyZone:Create({
+    vector2(926.54, -2347.98),
+    vector2(937.45, -2348.98),
+    vector2(936.37, -2361.87),
+    vector2(925.53, -2360.96),
+  }, {
+    name="zone",
+    minZ = 20.0,
+    maxZ = 25.0,
+    debugPoly = true
+})
+
+zone:onPlayerInOut(function(isPointInside)
+    if isPointInside then  
+        N_0x4757f00bc6323cfe(`WEAPON_UNARMED`, 0.1) 
+    else
+        N_0x4757f00bc6323cfe(`WEAPON_UNARMED`, 1.0) 
+    end
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+client_scripts {
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/EntityZone.lua',
+    '@PolyZone/CircleZone.lua',
+    '@PolyZone/ComboZone.lua',
+    'client.lua'
+}
